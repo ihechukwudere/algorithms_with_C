9a1aeea4bcd69661ad1a4f3418c33a29d65bdc58
Ihechukwudere <ihechukwudere@yahoo.com>
2022-12-14 11:30:47 +0200

    Minimize sorting time

--- bubble_sort/main.c
+++ bubble_sort/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 void bubble_sort(int A[], int len);
 
@@ -15,11 +16,14 @@ int main(int argc, char const *argv[])
 
 void bubble_sort(int A[], int len)
 {
+    bool swapped = false;
+    int i = 0;
    // outer loop: controls the number of passes through the array
-    for (size_t i = 0; i < len; i++)
+    do
     {
+        swapped = false;
         // Inner loop: does the passes it and swaps the element
-        for (size_t j = 0; j < (len - 1); j++)
+        for (size_t j = 0; j < (len - 1 - i); j++)
         {
             // Sorts in ascending order
             //The less than (<) operator could be changed for sort in descending order.
@@ -28,8 +32,10 @@ void bubble_sort(int A[], int len)
                 int temp = A[j];
                 A[j] = A[j + 1];
                 A[j + 1] = temp;
+                swapped = true;
             }
         }
-        
-    } 
+        i++;
+    } while (swapped);
+    
 }
