66a84a76f429fafc64cb4626ebcc80df3f1134d5
Ihechukwudere <ihechukwudere@yahoo.com>
2022-12-14 11:01:34 +0200

    Sort array in ascending order

--- bubble_sort/main.c
+++ bubble_sort/main.c
@@ -2,6 +2,31 @@
 
 int main(int argc, char const *argv[])
 {
-    printf("Hello");
+    int A[] = {3, 5, 2, 6, 8, 0, 9, 8, 4, 7};
+    int len = 10;
+
+    // outer loop: controls the number of passes through the array
+    for (size_t i = 0; i < len; i++)
+    {
+        // Inner loop: does the passes it and swaps the element
+        for (size_t j = 0; j < (len - 1); j++)
+        {
+            // Sorts in ascending order
+            //The less than (<) operator could be changed for sort in descending order.
+            if(A[j] > A[j + 1])
+            {
+                int temp = A[j];
+                A[j] = A[j + 1];
+                A[j + 1] = temp;
+            }
+        }
+        
+    }
+
+    // Print array elements
+    for (size_t i = 0; i < len; i++)
+        printf("A[%zu] = %d\n", i, A[i]);
+    
+    
     return 0;
 }
